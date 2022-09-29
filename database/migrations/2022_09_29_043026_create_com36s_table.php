f6746b466eb42af652f8b2088c9fa2b314c0d385
Alexis-Chata <alexizz.19.ac@gmail.com>
2022-09-28 23:50:16 -0500

    migraciones

--- database/migrations/2022_09_29_043026_create_com36s_table.php
+++ database/migrations/2022_09_29_043026_create_com36s_table.php
@@ -15,6 +15,33 @@ class CreateCom36sTable extends Migration
     {
         Schema::create('com36s', function (Blueprint $table) {
             $table->id();
+            $table->string("CZON")->nullable()->default("-");
+            $table->string("CRUT")->nullable()->default("-");
+            $table->string("CLETD")->nullable()->default("-");
+            $table->string("CTIP")->nullable()->default("-");
+            $table->string("NFAC")->nullable()->default("-");
+            $table->string("CVEN")->nullable()->default("-");
+            $table->string("CCLI")->nullable()->default("-");
+            $table->string("FMOV")->nullable()->default("-");
+            $table->string("FEMI")->nullable()->default("-");
+            $table->string("TNOMREP")->nullable()->default("-");
+            $table->string("CRUCCLI")->nullable()->default("-");
+            $table->string("TDIR")->nullable()->default("-");
+            $table->string("QDESIGV")->nullable()->default("-");
+            $table->string("QIMPTOT")->nullable()->default("-");
+            $table->string("QIMPVTA")->nullable()->default("-");
+            $table->string("QIMPCOM")->nullable()->default("-");
+            $table->string("FVEN")->nullable()->default("-");
+            $table->string("TVEN")->nullable()->default("-");
+            $table->string("CRUCCIA")->nullable()->default("-");
+            $table->string("FLIQ")->nullable()->default("-");
+            $table->string("CTIPLIQ")->nullable()->default("-");
+            $table->string("NPED")->nullable()->default("-");
+            $table->string("CMOD")->nullable()->default("-");
+            $table->string("CLISTPR")->nullable()->default("-");
+            $table->string("CUSER")->nullable()->default("-");
+            $table->string("FUPGR")->nullable()->default("-");
+            $table->string("TUPGR")->nullable()->default("-");
             $table->timestamps();
         });
     }
--- database/migrations/2022_09_29_043045_create_com37s_table.php
+++ database/migrations/2022_09_29_043045_create_com37s_table.php
@@ -15,6 +15,23 @@ class CreateCom37sTable extends Migration
     {
         Schema::create('com37s', function (Blueprint $table) {
             $table->id();
+            $table->string("FMOV")->nullable()->default("-");
+            $table->string("CCLI")->nullable()->default("-");
+            $table->string("QPREUNI")->nullable()->default("-");
+            $table->string("QIMP")->nullable()->default("-");
+            $table->string("CCODART")->nullable()->default("-");
+            $table->string("QCANPED")->nullable()->default("-");
+            $table->string("TDES")->nullable()->default("-");
+            $table->string("CITEM")->nullable()->default("-");
+            $table->string("CPROM")->nullable()->default("-");
+            $table->string("CLETD")->nullable()->default("-");
+            $table->string("CTIP")->nullable()->default("-");
+            $table->string("NFAC")->nullable()->default("-");
+            $table->string("NPED")->nullable()->default("-");
+            $table->string("CLISTPR")->nullable()->default("-");
+            $table->string("CUSER")->nullable()->default("-");
+            $table->string("FUPGR")->nullable()->default("-");
+            $table->string("TUPGR")->nullable()->default("-");
             $table->timestamps();
         });
     }
