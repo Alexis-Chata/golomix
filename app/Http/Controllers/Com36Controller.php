5ff8fd14a1d4476102b3b016ccbf83911d36aa27
Alexis-Chata <alexizz.19.ac@gmail.com>
2022-10-22 01:50:05 -0500

    tratando de elminar pedidos eliminados

--- app/Http/Controllers/Com36Controller.php
+++ app/Http/Controllers/Com36Controller.php
@@ -7,6 +7,7 @@ use App\Imports\Com36sImport;
 use App\Imports\Com37sImport;
 use App\Models\Com36;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Facades\Excel;
 
 class Com36Controller extends Controller
@@ -39,6 +40,30 @@ class Com36Controller extends Controller
      */
     public function store(Request $request)
     {
+        $f = fopen($request->file('arch_com36'), 'r');
+        $npeds = array();
+        while (($data = fgetcsv($f)) !== false) {
+
+            if(!isset($indiceNped)){
+                $fila = $data;
+            }
+
+            foreach ($fila as $key => $value){
+
+                if ($value=='NPED'){
+                    $indiceNped = $key;
+                }
+                if ($value=='FUPGR'){
+                    $indiceFupgr = $key;
+                }
+            }
+
+            if(isset($indiceNped) && $data[$indiceNped]!='NPED'){
+                $npeds[] = $data[$indiceNped];
+                $fupgr = Carbon::createFromFormat('d/m/Y', $data[$indiceFupgr]);
+            }
+        }
+        Com36::whereNotIn('nped', $npeds)->where('fupgr', $fupgr->format('Y/m/d'))->delete();
         //dd($request->arch_com36);
         $archivo = $request->file('arch_com36');
         Excel::import(new Com36sImport, $archivo);
