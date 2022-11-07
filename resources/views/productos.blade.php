75abc66a270f4679e69fa92a58dae57de4a1257e
Alexis-Chata <alexizz.19.ac@gmail.com>
2022-11-07 08:41:35 -0500

    title producto

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Productos</title>
+    <title>Productos {{ $precioMayorista ? 'Mayorista' : 'Bodega' }}</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <style>
         body{
