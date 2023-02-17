487c704497a113343a03c45febb910ea1ba3830e
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-02-17 08:59:14 -0500

    mejora temporal listacliente x tiempo de carga

--- resources/views/listaClientes.blade.php
+++ resources/views/listaClientes.blade.php
@@ -115,8 +115,8 @@
                 <th style="text-align: center;">Codigo Cliente</th>
                 <th style="text-align: center;">Nombre Cliente</th>
                 <th style="text-align: center;">Direccion Cliente</th>
-                <th style="text-align: center;">RUC / DNI</th>
-                <th style="text-align: center;">Ult. Compra</th>
+                {{-- <th style="text-align: center;">RUC / DNI</th>
+                <th style="text-align: center;">Ult. Compra</th> --}}
                 <th style="text-align: center;">Modulo</th>
                 <th style="text-align: center;">Zona</th>
                 <th style="text-align: center;">LP</th>
@@ -131,8 +131,8 @@
                     <td>{{ $com31->ccli }}</td>
                     <td>{{ $com31->com07s->tcli }}</td>
                     <td>{{ $com31->com07s->tdir }}</td>
-                    <td>{{ $com31->com07s->cruc ?? $com31->com07s->le }}</td>
-                    <td>{{ $com31->scrhcom20s->count() ? $com31->scrhcom20s->last()->femi : '-' }}</td>
+                    {{-- <td>{{ $com31->com07s->cruc ?? $com31->com07s->le }}</td>
+                    <td>{{ $com31->scrhcom20s->count() ? $com31->scrhcom20s->last()->femi : '-' }}</td> --}}
                     <td>{{ $com31->cmod }}</td>
                     <td>{{ $com31->com30s->czon }}</td>
                     <td>{{ $com31->com07s->clistpr }}</td>
@@ -147,8 +147,8 @@
                 <th style="text-align: center;">Codigo Cliente</th>
                 <th style="text-align: center;">Nombre Cliente</th>
                 <th style="text-align: center;">Direccion Cliente</th>
-                <th style="text-align: center;">RUC / DNI</th>
-                <th style="text-align: center;">Ult. Compra</th>
+                {{-- <th style="text-align: center;">RUC / DNI</th>
+                <th style="text-align: center;">Ult. Compra</th> --}}
                 <th style="text-align: center;">Modulo</th>
                 <th style="text-align: center;">Zona</th>
                 <th style="text-align: center;">LP</th>
