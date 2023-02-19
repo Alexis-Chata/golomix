8aba65247bf231110830ea8c90bf48e06e224c40
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-02-19 07:20:33 -0500

    mejoras

--- resources/views/listaClientes.blade.php
+++ resources/views/listaClientes.blade.php
@@ -28,6 +28,29 @@
         div#example_wrapper {
             margin-top: 12px;
         }
+
+        svg{
+            width: 18px;
+            fill: white;
+        }
+
+        span{
+            margin: 0;
+            padding: 0;
+            color: white;
+        }
+        button{
+            display: flex;
+            align-items: center;
+            gap: 8px;
+            padding: 5px;
+            border-color: #ec4537;
+            background-color: #ec4537;
+            border-radius: 5px;
+        }
+        a{
+            text-decoration: none
+        }
     </style>
 </head>
 
@@ -39,7 +62,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr1->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr1->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span> {{ $com10s->com30sr1->crut }} </span></button>
                     </a>
                 </div>
             </div>
@@ -49,7 +72,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr2->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr2->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr2->crut }}</span></button>
                     </a>
                 </div>
             </div>
@@ -59,7 +82,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr3->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr3->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr3->crut }}</span></button>
                     </a>
                 </div>
             </div>
@@ -69,7 +92,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr4->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr4->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr4->crut }}</span></button>
                     </a>
                 </div>
             </div>
@@ -79,7 +102,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr5->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr5->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr5->crut }}</span></button>
                     </a>
                 </div>
             </div>
@@ -89,7 +112,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr6->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr6->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr6->crut }}</span></button>
                     </a>
                 </div>
             </div>
@@ -99,7 +122,7 @@
                 <div class="col-xl-12 text-right">
                     <a href="{{ route('listaclientesDownload-pdf', [$cven, $com10s->com30sr7->crut]) }}"
                         class="btn btn-success btn-sm">
-                        <button type="button" class="pointer">{{ $com10s->com30sr7->crut }}</button>
+                        <button type="button" class="pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V304H296 272 184 160c-35.3 0-64 28.7-64 64v80 48 16H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM160 352h24c30.9 0 56 25.1 56 56s-25.1 56-56 56h-8v32c0 8.8-7.2 16-16 16s-16-7.2-16-16V448 368c0-8.8 7.2-16 16-16zm24 80c13.3 0 24-10.7 24-24s-10.7-24-24-24h-8v48h8zm88-80h24c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H272c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16zm24 128c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16h-8v96h8zm72-112c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v32h32c8.8 0 16 7.2 16 16s-7.2 16-16 16H400v48c0 8.8-7.2 16-16 16s-16-7.2-16-16V432 368z"/></svg><span>{{ $com10s->com30sr7->crut }}</span></button>
                     </a>
                 </div>
             </div>
