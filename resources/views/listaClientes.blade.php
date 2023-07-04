579f8845e80c2d3bc5140654fed01812b218c6e6
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-07-03 21:37:26 -0500

    Mejora

--- resources/views/listaClientes.blade.php
+++ resources/views/listaClientes.blade.php
@@ -194,16 +194,6 @@
         });
     </script>
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M5GTN7JJ32"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-M5GTN7JJ32');
-    </script>
-
 </body>
 
 </html>
--- resources/views/pedidos.blade.php
+++ resources/views/pedidos.blade.php
@@ -127,16 +127,6 @@
         </details>
     @endforeach
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M5GTN7JJ32"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-M5GTN7JJ32');
-    </script>
-
 </body>
 
 </html>
