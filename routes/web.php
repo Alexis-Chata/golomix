44d56a15111386ee6af4e18069cd25008853343c
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-09-26 09:52:39 -0500

    Avance

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,33 @@ Route::middleware([
         //dd($com10s);
         return view('vendedorRutas', compact('com10s'));
     })->name('allVendedorRutas');
+
+
+    Route::get('/pedidos', function () {
+        $fupgr = Com36::latest('fupgr')->first()->fupgr;
+        $com36s = Com36::with(['com37s', 'com30s'])->where('fupgr', $fupgr)->get();
+        $pedidosAgrupados = $com36s->sortBy(['cven', 'ccli'])->groupBy(['cven', 'tven', 'crut'], $preserveKeys = true);
+        $fupgr = Carbon::parse($fupgr)->format('d-m-Y');
+        $marcas = Ugr01::where('cind', '045')->get();
+        $com01s = Com01::all(['id', 'cequiv', 'tcor', 'qfaccon', 'cc04'])->keyBy('cequiv')->sort();
+        //return $com01s;
+        $com37s = Com37::whereIn('nped', $com36s->pluck('nped'))->get(['id', 'nped', 'ccodart', 'tdes', 'qcanped', 'qpreuni', 'qimp', 'cprom'])->sortBy('ccodart');
+        $com37s = $com37s->groupBy('ccodart');
+        $com37s->each(function ($item, $key) use ($com01s, $marcas) {
+            $unidadMedida = $com01s[substr($key, -3)]->qfaccon;
+
+            $sumaunidads = $item->sum('qcanpedunidads');
+            $sumaunidadsAbultos = intval($sumaunidads / $unidadMedida);
+            $sumaunidadsAbultosRestoenunidad = $sumaunidads % $unidadMedida;
+
+            $item->marcacod = ($com01s[substr($key, -3)]->cc04);
+            $item->marca = $marcas->where('ccod', $com01s[substr($key, -3)]->cc04)->first()->tdes;
+            $item->totalqcanpedbultos = $item->sum('qcanpedbultos') + $sumaunidadsAbultos;
+            $item->totalqcanpedunidads = str_pad($sumaunidadsAbultosRestoenunidad, 2, 0, STR_PAD_LEFT);
+        });
+        //return dd($com37s->first());
+        return view('pedidos', compact('com36s', 'pedidosAgrupados', 'fupgr', 'com37s'));
+    })->name('allpedidos');
 });
 
 Route::get('/listaclientes', function () {
@@ -132,32 +159,6 @@ Route::get('/listaclientes/{cven}', function ($cven) {
 })->name('listaclientesXvendedor');
 
 Route::get('listaclientes-download-pdf/{cven}/{crut}', [Com10Controller::class, 'listaclientesDownloadPdf'])->name('listaclientesDownload-pdf');
-
-Route::get('/pedidos', function () {
-    $fupgr = Com36::latest('fupgr')->first()->fupgr;
-    $com36s = Com36::with(['com37s', 'com30s'])->where('fupgr', $fupgr)->get();
-    $pedidosAgrupados = $com36s->sortBy(['cven', 'ccli'])->groupBy(['cven', 'tven', 'crut'], $preserveKeys = true);
-    $fupgr = Carbon::parse($fupgr)->format('d-m-Y');
-    $marcas = Ugr01::where('cind', '045')->get();
-    $com01s = Com01::all(['id', 'cequiv', 'tcor', 'qfaccon', 'cc04'])->keyBy('cequiv')->sort();
-    //return $com01s;
-    $com37s = Com37::whereIn('nped', $com36s->pluck('nped'))->get(['id', 'nped', 'ccodart', 'tdes', 'qcanped', 'qpreuni', 'qimp', 'cprom'])->sortBy('ccodart');
-    $com37s = $com37s->groupBy('ccodart');
-    $com37s->each(function ($item, $key) use ($com01s, $marcas) {
-        $unidadMedida = $com01s[substr($key, -3)]->qfaccon;
-
-        $sumaunidads = $item->sum('qcanpedunidads');
-        $sumaunidadsAbultos = intval($sumaunidads / $unidadMedida);
-        $sumaunidadsAbultosRestoenunidad = $sumaunidads % $unidadMedida;
-
-        $item->marcacod = ($com01s[substr($key, -3)]->cc04);
-        $item->marca = $marcas->where('ccod', $com01s[substr($key, -3)]->cc04)->first()->tdes;
-        $item->totalqcanpedbultos = $item->sum('qcanpedbultos') + $sumaunidadsAbultos;
-        $item->totalqcanpedunidads = str_pad($sumaunidadsAbultosRestoenunidad, 2, 0, STR_PAD_LEFT);
-    });
-    //return dd($com37s->first());
-    return view('pedidos', compact('com36s', 'pedidosAgrupados', 'fupgr'));
-})->name('allpedidos');
 
 Route::get('/pedidos/transporte', function () {
     $fupgr = Com36::latest('fupgr')->first()->fupgr;
