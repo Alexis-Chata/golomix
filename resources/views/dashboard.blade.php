5460c7664251fe003c41f0e2cbf0749a31204e3a
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-05 12:13:50 -0500

    Avance

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,86 @@
         </h2>
     </x-slot>
 
+    <!-- Loading CSS -->
+    <style>
+        .loading {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 9999;
+            transition: 1s all;
+            opacity: 0;
+        }
+
+        .loading.show {
+            opacity: 1;
+        }
+
+        .loading .spin {
+            border: 3px solid hsla(185, 100%, 62%, 0.2);
+            border-top-color: #3cefff;
+            border-radius: 50%;
+            width: 3em;
+            height: 3em;
+            animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+            to {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
+
+    <!-- Loading HTML -->
+    <div class="loading show">
+        <div class="spin"></div>
+    </div>
+
+    <!-- Loading Javascript -->
+    <script>
+        // Loading
+        var Loading = (loadingDelayHidden=0) => {
+            let loading=null;
+            const myLoadingDelayHidden=loadingDelayHidden;
+            let imgs=[];
+            let lenImgs=0;
+            let counterImgsLoading=0;
+            function incrementCounterImgs(){
+                counterImgsLoading+=1;
+                if(counterImgsLoading===lenImgs){
+                    hideLoading()
+                }
+            }
+
+            function hideLoading(){
+                if(loading!==null){
+                    loading.classList.remove('show');
+                    setTimeout(function(){loading.remove()},myLoadingDelayHidden)}}
+                    function init(){
+                        document.addEventListener('DOMContentLoaded',function(){
+                            loading = document.querySelector('.loading');
+                            imgs=Array.from(document.images);
+                            lenImgs=imgs.length;
+                            if(imgs.length===0){
+                                hideLoading()
+                            }else{
+                                imgs.forEach(function(img){img.addEventListener('load',incrementCounterImgs,false)})
+                            }
+                        })
+                    }
+                        return{'init':init}
+        }
+
+        Loading(1000).init();
+    </script>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
@@ -18,7 +98,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Pedidos">
-                                    <form action="{{ route('com36.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com36.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com36" class="m-4">Subir archivo Com36 </label>
                                         <input type="file" name="arch_com36" id="arch_com36" required class="m-4">
@@ -34,7 +115,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Detalle Pedidos">
-                                    <form action="{{ route('com37.store') }}" class="py-6" method="post" enctype="multipart/form-data" >
+                                    <form action="{{ route('com37.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com37" class="m-4">Subir archivo Com37 </label>
                                         <input type="file" name="arch_com37" id="arch_com37" required class="m-4">
@@ -50,7 +132,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Rutas">
-                                    <form action="{{ route('com30.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com30.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com30" class="m-4">Subir archivo Com30 </label>
                                         <input type="file" name="arch_com30" id="arch_com30" required class="m-4">
@@ -66,7 +149,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Productos">
-                                    <form action="{{ route('com01.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com01.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com01" class="m-4">Subir archivo Com01 </label>
                                         <input type="file" name="arch_com01" id="arch_com01" required class="m-4">
@@ -82,7 +166,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Marca de Productos">
-                                    <form action="{{ route('ugr01.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('ugr01.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_ugr01" class="m-4">Subir archivo Ugr01 </label>
                                         <input type="file" name="arch_ugr01" id="arch_ugr01" required class="m-4">
@@ -98,7 +183,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Clientes">
-                                    <form action="{{ route('com07.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com07.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com07" class="m-4">Subir archivo Com07 </label>
                                         <input type="file" name="arch_com07" id="arch_com07" required class="m-4">
@@ -114,7 +200,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Cliente-Ruta">
-                                    <form action="{{ route('com31.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com31.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com31" class="m-4">Subir archivo Com31 </label>
                                         <input type="file" name="arch_com31" id="arch_com31" required class="m-4">
@@ -130,7 +217,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Vendedor-Rutas-Zona">
-                                    <form action="{{ route('com10.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com10.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com10" class="m-4">Subir archivo Com10 </label>
                                         <input type="file" name="arch_com10" id="arch_com10" required class="m-4">
@@ -146,7 +234,8 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Consesioranio">
-                                    <form action="{{ route('com05.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com05.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_com05" class="m-4">Subir archivo Com05 </label>
                                         <input type="file" name="arch_com05" id="arch_com05" required class="m-4">
@@ -162,10 +251,13 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Actualiza Tipo Producto">
-                                    <form action="{{ route('com01.actualizaTipoProductoId') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('com01.actualizaTipoProductoId') }}" class="py-6"
+                                        method="post" enctype="multipart/form-data">
                                         @csrf
-                                        <label for="com01_actualizaTipoProducto" class="m-4">Actualizar Tipo Producto </label>
-                                        <input type="file" name="com01_actualizaTipoProducto" id="com01_actualizaTipoProducto" required class="m-4">
+                                        <label for="com01_actualizaTipoProducto" class="m-4">Actualizar Tipo Producto
+                                        </label>
+                                        <input type="file" name="com01_actualizaTipoProducto"
+                                            id="com01_actualizaTipoProducto" required class="m-4">
                                         <button type="submit"
                                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4 m-4">Subir
                                             Archivo</button>
@@ -178,10 +270,12 @@
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                                 <div class="row px-6" title="Actualiza el Tipo Producto">
-                                    <form action="{{ route('scrhcom20.store') }}" class="py-6" method="post" enctype="multipart/form-data">
+                                    <form action="{{ route('scrhcom20.store') }}" class="py-6" method="post"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         <label for="arch_scr_hcom20" class="m-4">Subir archivo ScrHcom20 </label>
-                                        <input type="file" name="arch_scr_hcom20" id="arch_scr_hcom20" required class="m-4">
+                                        <input type="file" name="arch_scr_hcom20" id="arch_scr_hcom20" required
+                                            class="m-4">
                                         <button type="submit"
                                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4 m-4">Subir
                                             Archivo</button>
@@ -195,18 +289,42 @@
         </div>
     </div>
 
-    <@push('eventsubmit-js')
+
+
+
+    @push('eventsubmit-js')
     <script>
+        document.body.addEventListener("keyup", function (event) {
+            // El parámetro del evento es del tipo KeyboardEvent
+            if (event.code === 'Escape' || event.keyCode === 27) {
+                console.log(event.code);
+                alert('Esc pulsado');
+                window.location.href="{{ route('dashboard') }}";
+            }
+        });
         document.body.addEventListener('submit', cargando);
 
         function cargando(event) {
-            const clickedElement = event.target;
 
-            buttons = document.querySelectorAll("body main button");
+        const clickedElement = event.target;
+        clickedElement.parentNode.classList.add("relative");
+        clickedElement.classList.add("opacity-50");
 
-            buttons.forEach((element) => element.disabled=true);
+        buttons = document.querySelectorAll("body main button");
+        inputs = document.querySelectorAll("body main input[type=file]");
 
-            console.log('hi');
+        buttons.forEach((element) => element.classList.add("pointer-events-none"));
+        inputs.forEach((element) => element.classList.add("pointer-events-none"));
+
+        const div = document.createElement("div");
+        div.innerHTML = '<div id="cargando" role="status" class="hidden absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"><svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg><span class="sr-only">Loading...</span></div>';
+        clickedElement.parentNode.appendChild(div);
+
+        clickedElement.parentNode.querySelector("div#cargando").classList.remove("hidden");
+
+        forms = document.querySelectorAll("main form");
+        forms.forEach((element) => element.classList.add("opacity-50"));
+
         }
     </script>
     @endpush
