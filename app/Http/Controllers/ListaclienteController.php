5a92195131337b7505fe827fca505d96937148fa
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-09 09:25:34 -0500

    Mejora

--- app/Http/Controllers/ListaclienteController.php
+++ app/Http/Controllers/ListaclienteController.php
@@ -18,7 +18,7 @@ class ListaclienteController extends Controller
 
     public function listaclientes($cven)
     {
-        (Gate::denies('verpedidos', $cven) ? abort(401, auth()->user()->codVendedorAsignados->pluck('cven')->first()) : 'Allowed');
+        (Gate::denies('verpedidos', $cven) ? abort(401, auth()->user()->codVendedorAsignados->pluck('cven')->first()??'') : 'Allowed');
 
         $com10s = Com10::with('com30sr1', 'com30sr2', 'com30sr3', 'com30sr4', 'com30sr5', 'com30sr6', 'com30sr7')->firstWhere('cven', $cven);
         //dd($com10s->toArray());
