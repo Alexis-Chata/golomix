ecbf8d79d2509d021901536aab093b6cdedaa18a
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-09 09:23:09 -0500

    Mejora menu

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -26,7 +26,7 @@
 
             @if (request()->routeIs('allpedidos') || request()->routeIs('pedidos'))
                 <br />
-                <div class="flex items-center pt-8 sm:justify-center sm:pt-0">
+                <div class="flex items-center pt-8 justify-center sm:pt-0">
                     <a href="{{ route('pedidos', $exception->getMessage()) }}"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ir
                         a Pedidos</a>
--- resources/views/livewire/custom/navigation-menu.blade.php
+++ resources/views/livewire/custom/navigation-menu.blade.php
@@ -182,80 +182,36 @@
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
 
-                <x-responsive-nav-link href="{{ route('allpedidos') }}" :active="request()->routeIs('allpedidos') || request()->routeIs('pedidos')">
-                    {{ __('Pedidos') }}
-                </x-responsive-nav-link>
+                @if ($cvens->count())
+
+                    @if ($cvens->count() > 1)
+                        <x-custom.responsive-nav-dropdown :cvens="$cvens" :com10s="$com10s" titulo="Pedidos" nameRoute="pedidos" />
+                    @else
+                        <x-responsive-nav-link href="{{ route('pedidos', $cvens->first()) }}" :active="request()->routeIs('allpedidos') || request()->routeIs('pedidos')">
+                            {{ __('Pedidos') }}
+                        </x-nav-link>
+                    @endif
+
+                @endif
+
                 <x-responsive-nav-link href="{{ route('allProductos') }}" :active="request()->routeIs('allProductos')">
                     {{ __('P.Bodega') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link href="{{ route('allProductosMayorista') }}" :active="request()->routeIs('allProductosMayorista')">
                     {{ __('P.Mayorista') }}
                 </x-responsive-nav-link>
-                <x-responsive-nav-link href="{{ route('listaclientes') }}" :active="request()->routeIs('listaclientes') || request()->routeIs('listaclientesXvendedor')">
-                    {{ __('Lista Clientes') }}
-                </x-responsive-nav-link>
 
-                <!-- Settings Dropdown -->
-                @php
-                    $classes_responsive_nav_link =
-                        request()->routeIs('listaclientesXvendedor') ?? false
-                            ? 'block w-full pl-3 pr-4 border-l-4 border-indigo-400 dark:border-indigo-600 text-left text-base
-                                    font-medium text-indigo-700 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none
-                                    focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900
-                                    focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out'
-                            : 'block w-full pl-3 pr-4 border-l-4 border-transparent text-left text-base font-medium
-                                    text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50
-                                    dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none
-                                    focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700
-                                    focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out';
-                @endphp
-                <div class="sm:flex sm:items-center sm:ml-6 {{ $classes_responsive_nav_link }}">
-                    <div class="relative">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button"
-                                        class="py-3 inline-flex items-center border border-transparent text-base leading-4 font-medium rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none dark:focus:bg-gray-700 dark:active:bg-gray-700 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
+                @if ($cvens->count())
 
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
+                    @if ($cvens->count() > 1)
+                        <x-custom.responsive-nav-dropdown :cvens="$cvens" :com10s="$com10s" titulo="Lista Clientes" nameRoute="listaclientesXvendedor" />
+                    @else
+                        <x-responsive-nav-link href="{{ route('listaclientesXvendedor', $cvens->first()) }}" :active="request()->routeIs('listaclientes') || request()->routeIs('listaclientesXvendedor')">
+                            {{ __('Lista Clientes') }}
+                        </x-responsive-nav-link>
+                    @endif
 
-                            <x-slot name="content">
-                                <!-- Account Management -->
-                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                    {{ __('Manage Account') }}
-                                </div>
-
-                                <x-dropdown-link href="{{ route('profile.show') }}">
-                                    {{ __('Profile') }}
-                                </x-dropdown-link>
-
-                                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                    <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                        {{ __('API Tokens') }}
-                                    </x-dropdown-link>
-                                @endif
-
-                                <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}" x-data>
-                                    @csrf
-
-                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-
-                </div>
+                @endif
 
                 <x-responsive-nav-link href="{{ route('allpedidosXtransporte') }}" :active="request()->routeIs('allpedidosXtransporte')">
                     {{ __('Distribucion de Carga') }}
