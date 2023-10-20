5a6d980207ab694f7cd59408ce75495a0c0d6e2f
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-20 09:24:48 -0500

    Avance

--- resources/views/tomador/index.blade.php
+++ resources/views/tomador/index.blade.php
@@ -3,36 +3,43 @@
 @section('title', 'Index')
 
 @section('content_header')
-    <h1>Index</h1>
+<h1>Lista Clientes</h1>
 @stop
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            @forelse($clientes as $key => $cliente)
-                <div class="col-md-6">
-                    <div class="info-box">
-                        <div class="info-box-content">
-                            <span class="info-box-number">#{{ $cliente->ccli }} - {{ $cliente->tcli }}</span>
-                            <span class="info-box-text">{{ $cliente->tdir }}</span>
-                        </div>
-                        <span class="info-box-icon bg-info">
-                            <i class="fas fa-address-card"></i>
-                        </span>
-                    </div>
+<div class="container">
+    <div class="row">
+        @forelse($clientes as $key => $cliente)
+        <div class="col-md-6">
+            <div class="info-box">
+                <div class="info-box-content">
+                    <span class="info-box-number">#{{ $cliente->ccli }} - {{ $cliente->tcli }}</span>
+                    <span class="info-box-text">
+                        <ul class="m-0 pl-4">
+                            <li>
+                                {{ $cliente->crut }} - {{ $cliente->tdes }}</li>
+                        </ul>
+                    </span>
+                    <span class="info-box-text">{{ $cliente->tdir }}</span>
                 </div>
-            @empty
-            @endforelse
+                <span class="info-box-icon bg-info">
+                    <i class="fas fa-address-card"></i>
+                </span>
+            </div>
         </div>
+        @empty
+        @endforelse
     </div>
+</div>
 @stop
 
 @section('css')
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+{{--
+<link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
 
 @section('js')
-    <script>
-        console.log('Hi!');
-    </script>
+<script>
+    console.log('Hi!');
+</script>
 @stop
