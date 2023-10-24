2594544ee94269f545918516dda105352e14bda2
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-24 14:56:30 -0500

    Avance

--- database/migrations/2023_10_24_110856_create_bitacoras_table.php
+++ database/migrations/2023_10_24_110856_create_bitacoras_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
         Schema::create('bitacoras', function (Blueprint $table) {
             $table->id();
             $table->string("descripcion");
+            $table->string("user_id");
+            $table->string("user_name");
+            $table->string("roles_names");
+            $table->string("permisos_names");
             $table->string("clase_metodo");
             $table->string("url");
             $table->string("url_metodo");
             $table->string("route_name");
             $table->string("ip");
-            $table->string("user_id");
-            $table->string("user_name");
-            $table->string("roles_names");
-            $table->string("permisos_names");
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
