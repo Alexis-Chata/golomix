c55617272757a3c245a3b497f10823cc3c2caf18
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-10-25 21:09:56 -0500

    Mejora

--- resources/views/bitacora.blade.php
+++ resources/views/bitacora.blade.php
@@ -71,6 +71,9 @@
             order: [[0, 'desc']],
             responsive: true,
             autoWidth: false,
+            "language": {
+                        "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
+                    }
         });
     </script>
 @stop
