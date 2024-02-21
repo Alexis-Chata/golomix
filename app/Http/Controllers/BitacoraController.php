340f8d4fd4faf29a836fecda04f4d138424316aa
Alexis-Chata <alexizz.19.ac@gmail.com>
2024-02-21 10:48:54 -0500

    Avance

--- app/Http/Controllers/BitacoraController.php
+++ app/Http/Controllers/BitacoraController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class BitacoraController extends Controller
 {
     public function bitacora(){
-        $bitacoras = Bitacora::orderBy('id', 'DESC')->get();
+        $fechaInicio = now()->subMonths(3)->startOfDay();
+        $bitacoras = Bitacora::whereDate('created_at', '>=', $fechaInicio)->orderBy('id', 'DESC')->get();
         return view('bitacora', compact('bitacoras'));
     }
 }
