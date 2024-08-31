58b95480e8fda7300b44f936f2aa7d2083b2cbbe
Alexis-chata <alexizz.19.ac@gmail.com>
2024-08-31 00:59:58 -0500

    Avance

--- resources/views/powerbi/powerbi.blade.php
+++ resources/views/powerbi/powerbi.blade.php
@@ -21,7 +21,8 @@
                             <div class="bg-white shadow-xl sm:rounded-lg">
                                 <div id="pedidos" class="p-2 bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg">
                                 <iframe class="m-auto" title="Dashboard Servicio al cliente" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNDYyMDQzN2UtZWM5NS00MzkyLTk5MTktMTIyMTc3NTdiNDc0IiwidCI6IjFiNDEyZTAzLTVmNmEtNGE2Zi1hNjk0LTM0M2I4NjYxNjRlZiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
-                                </div>
+                                <iframe class="m-auto" title="scrhcom21" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZmUyMGE4ZTAtY2U1ZS00M2FjLWE4MzItYjRhNmExZDkzZmRjIiwidCI6IjFiNDEyZTAzLTVmNmEtNGE2Zi1hNjk0LTM0M2I4NjYxNjRlZiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+                            </div>
                             </div>
                         </div>
                     </div>
