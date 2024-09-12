4cb70bbaaa9faff323df2de8834eda8be2ccbb38
Alexis-chata <alexizz.19.ac@gmail.com>
2024-09-12 02:30:09 -0500

    Avance

--- resources/views/components/filtrochart.blade.php
+++ resources/views/components/filtrochart.blade.php
@@ -17,11 +17,12 @@
     <div class="row flex justify-start col-md-3 gap-4 p-2.5 content-center flex items-center">
         <div class="col-md-3">
             <label for="from">De:</label>
-            <input type="date" id="from" name="from" class="form-control">
+            <input type="date" id="from" name="from" class="form-control bg-white border border-gray-300 text-gray-700 py-2 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+
         </div>
         <div class="col-md-3">
             <label for="to">AL</label>
-            <input type="date" id="to" name="to" class="form-control">
+            <input type="date" id="to" name="to" class="form-control bg-white border border-gray-300 text-gray-700 py-2 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
         </div>
         <div class="col-md-3">
             <button id="aplicar" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded form-control">Aplicar</button>
